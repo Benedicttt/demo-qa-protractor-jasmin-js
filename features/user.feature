ed105051089fe28a3176b2c5b790624d3e0fe54b
Sergey Kievskiy <s.kievskiy@404-group.com>
2018-08-20 13:56:07 +0300

    fix

--- features/user.feature
+++ features/user.feature
@@ -1,5 +1,7 @@
 Feature: Smoke test for SPOK project
+
 Scenario: Create User
+
   Given I open in Google page Spok project
   Then the title is "SPOK"
   And find login and password inputs
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -3,88 +3,62 @@ const chromedriver = require('chromedriver');
 const geckodriver = require('geckodriver');
 
 require('nightwatch-cucumber')({
-  cucumberArgs: ['--require', 'features/step_definitions','--format', 'node_modules/cucumber-pretty', '--format', 'json:reports/cucumber-report.json', 'features']
+  cucumberArgs: ['--require', 'features/step_definitions','--format', './node_modules/cucumber-pretty', '--format', 'json:reports/cucumber.json', 'features']
 });
 
 module.exports = {
-  src_folders: "features",
   output_folder: 'reports',
   custom_assertions_path: '',
   live_output: false,
   disable_colors: false,
-  test_workers: true,
   selenium: {
     start_process: true,
     server_path: seleniumServer.path,
-    log_path: 'logs/',
+    log_path: '',
     host: '127.0.0.1',
-    port: 4444,
-    // cli_args: {
-    //   "webdriver.chrome.driver": "./node_modules/chromedriver/bin/chromedriver",
-    //   "webdriver.gecko.driver": "./node_modules/geckodriver/bin/geckodriver"
-    // }
+    port: 4444
   },
   test_settings: {
     default: {
-      launch_url: 'http://localhost:3000',
+      launch_url: 'http://localhost:8087',
       selenium_port: 4444,
       selenium_host: '127.0.0.1',
       desiredCapabilities: {
-        browserName: 'firefox',
-			  marionette: true
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['incognito', 'headless', 'no-sandbox', 'disable-gpu']
+        }
       },
-      screenshots : {
-        enabled: true,
-        on_error: true,
-        on_failure: true,
-        path: './screenshots/default_firefox'
-      },
+      selenium: {
+        cli_args: {
+          'webdriver.chrome.driver': chromedriver.path
+        }
+      }
     },
     chrome: {
-			desiredCapabilities: {
-        browserName: "chrome",
-				javascriptEnabled: true
-      },
-      screenshots : {
-        enabled: true,
-        on_error: true,
-        on_failure: true,
-        path: './screenshots/chrome'
-      }			
-		},
-
-		chrome_headless: {
       desiredCapabilities: {
-        browserName: "chrome",
+        browserName: 'chrome',
         javascriptEnabled: true,
-        chromeOptions: {
-          args: [
-              "headless",
-              "disable-web-security",
-              "ignore-certificate-errors",
-              "no-sandbox", 
-              "disable-gpu"
-            ]
-          },
+        acceptSslCerts: true
       },
-      screenshots : {
-        enabled: true,
-        on_error: true,
-        on_failure: true,
-        path: './screenshots/chrome_headless'
+      selenium: {
+        cli_args: {
+          'webdriver.chrome.driver': chromedriver.path
+        }
       }
-		},
-
-		firefox: {
-			desiredCapabilities: {
-			  browserName: "firefox",
-			  marionette: true
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        marionette: true
       },
-      screenshots : {
-        enabled: true,
-        on_error: true,
-        on_failure: true,
-        path: './screenshots/firefox'
+      selenium: {
+        cli_args: {
+          'webdriver.gecko.driver': geckodriver.path
+        }
       }
     }
   }
