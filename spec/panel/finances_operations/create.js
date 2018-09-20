eb42a12ddbbf6a41018cf835a10ac636e1b6a935
Sergey Kievskiy <s.kievskiy@404-group.com>
2018-09-20 18:15:23 +0300

    recomment describe operations

--- spec/panel/finances_operations/create.js
+++ spec/panel/finances_operations/create.js
@@ -1,36 +1,36 @@
 describe('Finances receipts', () => {
-    // describe('Create receipt low', () => {
-    //     beforeAll( () => {
-    //         user_object.authorization(helper.user_email_last());
-    //     });
+    describe('Create receipt low', () => {
+        beforeAll( () => {
+            user_object.authorization(helper.user_email_last());
+        });
         
-    //     it(`Go to page and check title ${page.receipts.title}`,  () => {
-    //         go(page.receipts.get);
-    //         expect(browser.getTitle()).toEqual(page.receipts.title);
-    //     });
+        it(`Go to page and check title ${page.receipts.title}`,  () => {
+            go(page.receipts.get);
+            expect(browser.getTitle()).toEqual(page.receipts.title);
+        });
     
     
-    //     receipts_shared.fill_data();
-    //     receipts_shared.buttons();
-    //     receipts_shared.check_data_popup("SERVICE");
-    //     receipts_shared.check_data_popup("DDS");
-    // });
+        receipts_shared.fill_data();
+        receipts_shared.buttons();
+        receipts_shared.check_data_popup("SERVICE");
+        receipts_shared.check_data_popup("DDS");
+    });
 
-    // describe('Create receipt plus `return`', () => {
-    //     beforeAll( () => {
-    //         user_object.authorization(helper.user_email_last());
-    //     });
+    describe('Create receipt plus `return`', () => {
+        beforeAll( () => {
+            user_object.authorization(helper.user_email_last());
+        });
         
-    //     it(`Go to page and check title ${page.receipts.title}`,  () => {
-    //         go(page.receipts.get);
-    //         expect(browser.getTitle()).toEqual(page.receipts.title);
-    //     });
+        it(`Go to page and check title ${page.receipts.title}`,  () => {
+            go(page.receipts.get);
+            expect(browser.getTitle()).toEqual(page.receipts.title);
+        });
     
-    //     receipts_shared.fill_data();
-    //     receipts_shared.fill_data_return();
-    //     receipts_shared.buttons();
-    //     receipts_shared.check_data_popup("DDS", 0);
-    // });
+        receipts_shared.fill_data();
+        receipts_shared.fill_data_return();
+        receipts_shared.buttons();
+        receipts_shared.check_data_popup("DDS", 0);
+    });
 
     describe('Create receipt plus `NDS`', () => {
         beforeAll( () => {
