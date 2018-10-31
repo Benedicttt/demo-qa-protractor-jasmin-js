e340e480a4d4275a33dc22c1136092b70afc8e18
Sergey Kievskiy <s.kievskiy@404-group.com>
2018-10-31 15:27:14 +0300

    fix all test case

--- spec/panel/demands/service.js
+++ spec/panel/demands/service.js
@@ -1,22 +1,22 @@
 describe('Demands, create new, type `service`', () => {
 
-    fdescribe('case_1', () => {
+    describe('case_1', () => {
         demands_shared.run_test_case_service("case_1")
     });
 
-    fdescribe('case_2', () => {
+    describe('case_2', () => {
         demands_shared.run_test_case_service("case_2")
     });
 
-    fdescribe('case_3', () => {
+    describe('case_3', () => {
         demands_shared.run_test_case_service("case_3")
     });
 
-    fdescribe('case_4', () => {
+    describe('case_4', () => {
         demands_shared.run_test_case_service("case_4")
     });
 
-    fdescribe('case_5', () => {
+    describe('case_5', () => {
         demands_shared.run_test_case_service("case_5")
     });
 
