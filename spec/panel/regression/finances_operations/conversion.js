73e82332699cbb71e4370de9e0a66adaaa16ed0c
Sergey Kievskiy <s.kievskiy@404-group.com>
2019-02-04 17:06:04 +0300

    fix tests for new selecting fo currencies

--- spec/panel/regression/finances_operations/conversion.js
+++ spec/panel/regression/finances_operations/conversion.js
@@ -7,7 +7,7 @@ describe('Finances conversions', () => {
         conversion_shared.run_test_case("case_2")
     });
 
-    describe('case_3', () => {
+    fdescribe('case_3', () => {
         conversion_shared.run_test_case("case_3")
     });
 
