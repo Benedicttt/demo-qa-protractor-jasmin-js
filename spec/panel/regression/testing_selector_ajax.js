1ab98459b21823eb8246479de7163ad57587af6d
Sergey Kievskiy <s.kievskiy@404-group.com>
2019-05-01 02:35:44 +0300

    fix

--- spec/panel/regression/testing_selector_ajax.js
+++ spec/panel/regression/testing_selector_ajax.js
@@ -1,3 +1,7 @@
-fdescribe('Testing all value in selector contractor', () => {
+describe('Testing all value in selector contractor', () => {
     testing_selectors_shared.run_test_case("new", "services", "service")
+});
+
+fdescribe('Testing all value in selector contractor', () => {
+    testing_selectors_shared.run_test_case("new", "demands", "demand")
 });
--- spec/shared/testing_selectors.js
+++ spec/shared/testing_selectors.js
@@ -6,7 +6,8 @@ const scenarios = yaml.safeLoad(file);
 module.exports = {
     run_test_case: function(name_case, subject, type) {
         it(`Go to page and check title ${page[subject].title}`,  () => {
-            user_object.authorization(helper.user_email_last());
+            user_object.authorization(admin);
+            // user_object.authorization(helper.user_email_last());
             go(page[subject].new.get);
         });
 
@@ -14,19 +15,16 @@ module.exports = {
             let key = `${Object.keys(id)[0]}`;
 
             Object.values(id)[0].map(function (val) {
-                if (Object.keys(val)[0] === "0" ) {
+                let obj = Object.values(val)[0];
+                let id = Object.keys(obj)[0];
+                let value = Object.values(obj)[0];
 
+                if (id === "0") {
                     it(`{ ${type + "_" + key}: ${val} }`, () => {
                         browser.sleep(500);
                         tag_selector.selectOption(type + "_" + key, val);
                     })
-
                 } else {
-
-                    let obj = Object.values(val)[0];
-                    let id = Object.keys(obj)[0];
-                    let value = Object.values(obj)[0];
-
                     it(`{ ${type + "_" + key}: ${Object.keys(val)[0]} }`, () => {
                         browser.sleep(500);
                         tag_selector.selectOption(type + "_" + key, Object.keys(val)[0]);
@@ -34,7 +32,7 @@ module.exports = {
 
                     value.map(function (index, index_item) {
                         it(`{ ${type + "_" + id}: ${index} }`, () => {
-                            if (index_item === 0) { browser.sleep(1200); }
+                            if (index_item === 0) { browser.sleep(1000); }
 
                             tag_selector.selectOption(type + "_" + id, index)
                         });
