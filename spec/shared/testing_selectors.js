b87d509a175d5e64c46f7a71d36c6930b9e2577c
Sergey Kievskiy <s.kievskiy@404-group.com>
2019-04-30 22:45:30 +0300

    fix

--- spec/shared/testing_selectors.js
+++ spec/shared/testing_selectors.js
@@ -1,7 +1,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 let file = fs.readFileSync('spec/panel/test_case/testing_selectors/test_case.yml', 'utf8');
-const scenarios = yaml.safeLoad(file).service;
+const scenarios = yaml.safeLoad(file);
 
 module.exports = {
     run_test_case: function(name_case, subject) {
@@ -11,14 +11,16 @@ module.exports = {
             go(page[subject].new.get);
         });
 
-        // scenarios[`${name_case}`].selector.map(function (id) {
-        //     let value = `${Object.values(id)[0]}`;
-        //     let key = `${Object.keys(id)[0]}`;
-        //
-        //     it(`{ ${key}: ${value} }`, () => {
-        //         browser.sleep(500);
-        //         tag_selector.selectOption(key, value);
-        //     })
-        // });
+        scenarios[subject][name_case].selector.map(function (id) {
+            let value = `${Object.values(id)[0]}`;
+            let key = `${Object.keys(id)[0]}`;
+
+            Object.values(id)[0].map(function (val) {
+                it(`{ ${key}: ${val} }`, () => {
+                    browser.sleep(500);
+                    tag_selector.selectOption(key, val);
+                })
+            })
+        });
     }
 };
