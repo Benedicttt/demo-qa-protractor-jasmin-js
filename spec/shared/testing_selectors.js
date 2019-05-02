0422efd816cdfe697f6dae1bc0c8ec4bb8e8a72c
Sergey Kievskiy <s.kievskiy@404-group.com>
2019-05-03 02:40:28 +0300

    fix

--- spec/shared/testing_selectors.js
+++ spec/shared/testing_selectors.js
@@ -20,9 +20,9 @@ module.exports = {
             })
         });
 
-        scenarios[name_case][type].selector_filter_first_level.map(function (id) {
+        scenarios[name_case].selector_filter_first_level.map(function (id) {
             let value = `${Object.values(id)[0]}`;
-            let key = `${Object.keys(id)[0]}`;
+            let key = `filter_${Object.keys(id)[0]}`;
 
             value.split(",").map(function (index, index_item) {
                 it(`{ ${key} : ${index} }`, () => {
@@ -52,8 +52,7 @@ module.exports = {
             });
         });
 
-        scenarios[name_case][type].selector_filter_two_level.map(function (id) {
-
+        scenarios[name_case].selector_filter_two_level.map(function (id) {
             let key = `${Object.keys(id)[0]}`;
 
             Object.values(id)[0].map(function (val) {
@@ -62,20 +61,20 @@ module.exports = {
                 let value = Object.values(obj)[0];
 
                 if (id === "0") {
-                    it(`{ ${key}: ${val} }`, () => {
+                    it(`{ ${name_case + "_" + key}: ${val} }`, () => {
                         browser.sleep(500);
-                        tag_selector.selectOption(key, val);
+                        tag_selector.selectOption(name_case + "_" + key, val);
                     })
                 } else {
-                    it(`{ ${key}: ${Object.keys(val)[0]} }`, () => {
+                    it(`{ ${name_case + "_" + key}: ${Object.keys(val)[0]} }`, () => {
                         browser.sleep(500);
-                        tag_selector.selectOption(key, Object.keys(val)[0]);
+                        tag_selector.selectOption(name_case + "_" + key, Object.keys(val)[0]);
                     });
 
                     value.map(function (index, index_item) {
-                        it(`{ ${id}: ${index} }`, () => {
+                        it(`{ ${name_case + "_" + id}: ${index} }`, () => {
                             if (index_item === 0) { browser.sleep(1000); }
-                            tag_selector.selectOption(id, index)
+                            tag_selector.selectOption(name_case + "_" + id, index)
                         });
                     })
                 }
