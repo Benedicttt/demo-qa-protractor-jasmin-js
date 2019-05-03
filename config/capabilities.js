0c52898f00879ea3055fb1aee00087a47afb3060
Sergey Kievskiy <s.kievskiy@404-group.com>
2019-05-03 05:14:53 +0300

    add multithreading

--- config/capabilities.js
+++ config/capabilities.js
@@ -1,10 +1,74 @@
 module.exports =
 {
-    browserName: 'chrome',
-    chromeOptions: {
-        prefs: { 'credentials_enable_service': false },
-        args: [ "--disable-gpu", "--window-size=1920x1080" ]
-    }
-    // shardTestFiles: false,     // allows specs to be executed in parallel.
-    // maxInstances: 1,
+    set:
+    [
+        {
+            browserName: "chrome",
+            specs:
+                [
+                    "../spec/panel/preconditions/sign_up.js",
+                    "../spec/panel/preconditions/home_page.js",
+                    "../spec/panel/preconditions/sign_in.js",
+
+                    "../spec/panel/preconditions/user_access/set_user_access_full.js",
+
+                    "../spec/panel/preconditions/employee.js",
+
+                    "../spec/panel/preconditions/services/us.js",
+                    "../spec/panel/preconditions/services/we.js",
+
+                    "../spec/panel/regression/**/*.js"
+                ]
+        },
+
+        {
+            browserName: "chrome",
+            specs: [
+                "../spec/panel/regression/tests_selector/demand_new.js"
+            ]
+
+        },
+
+        {
+            browserName: "chrome",
+            specs: [
+                "../spec/panel/regression/tests_selector/service_new.js"
+            ]
+
+        },
+
+        {
+            browserName: "chrome",
+            specs: [
+                "../spec/panel/regression/tests_selector/filter_demand.js"
+            ]
+
+        },
+
+        {
+            browserName: "chrome",
+            specs: [
+                "../spec/panel/regression/tests_selector/filter_operations.js"
+            ]
+
+        },
+
+        {
+            browserName: "chrome",
+            specs: [
+                "../spec/panel/regression/tests_selector/filter_service.js"
+            ]
+
+        },
+
+        // {
+        //     browserName: "chrome",
+        //     specs: [
+        //         "../spec/panel/preconditions/cashier/cashier_real.js",
+        //         "../spec/panel/preconditions/cashier/cashier_virtual.js"
+        //     ]
+        //
+        // },
+    ]
 };
+
--- config/conf.js
+++ config/conf.js
@@ -6,32 +6,8 @@ let capabilities     =  require('./capabilities.js');
 
 exports.config = {
     // suites:       suites,
-    multiCapabilities: [
-        {
-            "browserName": "chrome",
-            count: 1,
-            specs:
-                [
-                    "../spec/panel/preconditions/sign_up.js",
-                    "../spec/panel/preconditions/home_page.js",
-                    "../spec/panel/preconditions/sign_in.js",
-                    "../spec/panel/preconditions/user_access/set_user_access_full.js",
-                    "../spec/panel/preconditions/cashier/cashier_real.js",
-                    "../spec/panel/preconditions/cashier/cashier_virtual.js",
-                    "../spec/panel/preconditions/employee.js",
-                    "../spec/panel/preconditions/services/us.js",
-                    "../spec/panel/preconditions/services/we.js",
-
-                    "../spec/panel/regression/**/*.js"
-                ]
-        },
-        {
-            "browserName": "chrome",
-            count: 1,
-            specs: ["../spec/panel/regression/testing_selector_ajax.js"]
-
-        },
-    ],
+    multiCapabilities: capabilities.set,
+    maxSessions: 3,
 
     framework:    'jasmine',
 
