bf27832b4561b12a35e0e8a28bb3c62fda037dc1
Sergey Kievskiy <s.kievskiy@404-group.com>
2019-05-03 06:55:42 +0300

    fix

--- config/capabilities.js
+++ config/capabilities.js
@@ -2,37 +2,37 @@ module.exports =
     {
         set:
             [
-                {
-                    browserName: "chrome",
-                    specs:
-                        [
-                            "../spec/panel/preconditions/sign_up.js",
-                            "../spec/panel/preconditions/home_page.js",
-                            "../spec/panel/preconditions/sign_in.js",
-
-                            "../spec/panel/preconditions/user_access/set_user_access_full.js",
-
-                            "../spec/panel/preconditions/employee.js"
-
-                        ]
-                },
-
-
-                {
-                    browserName: "chrome",
-                    specs: [
-                        "../spec/panel/regression/tests_selector/demand_new.js"
-                    ]
-
-                },
-
-                {
-                    browserName: "chrome",
-                    specs: [
-                        "../spec/panel/regression/tests_selector/service_new.js"
-                    ]
-
-                },
+                // {
+                //     browserName: "chrome",
+                //     specs:
+                //         [
+                //             "../spec/panel/preconditions/sign_up.js",
+                //             "../spec/panel/preconditions/home_page.js",
+                //             "../spec/panel/preconditions/sign_in.js",
+                //
+                //             "../spec/panel/preconditions/user_access/set_user_access_full.js",
+                //
+                //             "../spec/panel/preconditions/employee.js"
+                //
+                //         ]
+                // },
+                //
+                //
+                // {
+                //     browserName: "chrome",
+                //     specs: [
+                //         "../spec/panel/regression/tests_selector/demand_new.js"
+                //     ]
+                //
+                // },
+                //
+                // {
+                //     browserName: "chrome",
+                //     specs: [
+                //         "../spec/panel/regression/tests_selector/service_new.js"
+                //     ]
+                //
+                // },
 
                 {
                     browserName: "chrome",
@@ -43,29 +43,29 @@ module.exports =
 
                 },
 
-                {
-                    browserName: "chrome",
-                    specs: [
-                        "../spec/panel/regression/tests_selector/filter_demand.js"
-                    ]
-
-                },
-
-                {
-                    browserName: "chrome",
-                    specs: [
-                        "../spec/panel/regression/tests_selector/filter_operations.js"
-                    ]
-
-                },
-
-                {
-                    browserName: "chrome",
-                    specs: [
-                        "../spec/panel/regression/tests_selector/filter_service.js"
-                    ]
-
-                },
+                // {
+                //     browserName: "chrome",
+                //     specs: [
+                //         "../spec/panel/regression/tests_selector/filter_demand.js"
+                //     ]
+                //
+                // },
+                //
+                // {
+                //     browserName: "chrome",
+                //     specs: [
+                //         "../spec/panel/regression/tests_selector/filter_operations.js"
+                //     ]
+                //
+                // },
+                //
+                // {
+                //     browserName: "chrome",
+                //     specs: [
+                //         "../spec/panel/regression/tests_selector/filter_service.js"
+                //     ]
+                //
+                // },
 
                 // {
                 //     browserName: "chrome",
